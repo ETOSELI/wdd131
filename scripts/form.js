86d6598189c7cc38e57be8c0e9456d5a5d835cf5
etoseli <toselie3@gmail.com>
2024-12-07 20:41:37 +0300

    created my webpage for form

---
+++ scripts/form.js
@@ -0,0 +1,20 @@
+// Product array
+const products = [
+    { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+    { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+    { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+    { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+    { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
+];
+
+// Populate Product Select Options
+const productSelect = document.getElementById("product");
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+
+    const modified = document.lastModified;
+    document.getElementById("modified").textContent = modified;
+});
